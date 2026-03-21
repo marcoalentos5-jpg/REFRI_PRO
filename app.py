0bda091044c5361ef87a57f840fb553a4ecccd27
marcoalentos5-jpg <marcoalentos5@gmail.com>
2026-03-21 10:40:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -312,8 +312,7 @@ def main():
         renderizar_aba_1()  # <--- Aqui ele chama a função pela segunda vez no arquivo
 
 # LINHA 315
-    if __name__ == "__main__":
-    main()
+    if __name__ == "__main__": main()
 
 # FINALIZAÇÃO DO ARQUIVO - TOTAL DE LINHAS FISCALIZADAS: 257
 
