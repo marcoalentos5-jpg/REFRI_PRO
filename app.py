61ca570d7f0e66d37e7085c88d3006af9efec50f
marcoalentos5-jpg <marcoalentos5@gmail.com>
2026-03-27 07:41:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ def renderizar_aba_diagnosticos():
 
     st.markdown("---")
 
-O código 📦 Mestre_HVAC_Blindado_V1    
+O código Mestre_HVAC_Blindado_V1    
 
 # --- BLOCO 2: PROCESSAMENTO (CÁLCULOS) ---
     # Nota: No próximo passo, inseriremos a tabela PT aqui
