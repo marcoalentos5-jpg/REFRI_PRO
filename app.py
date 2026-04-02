3f217acfa17aeee53b0c8d4bd164e7ca6e8a6c6d
marcoalentos5-jpg <marcoalentos5@gmail.com>
2026-04-02 05:38:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -503,7 +503,7 @@ def renderizar_aba_2():
             use_container_width=True
         )
 
-        except Exception as e:
+            except Exception as e:
         # O except fica alinhado com o 'try' lá de cima (4 espaços ou 1 tab)
         st.error(f"❌ Erro na geração: Detalhe: {e}")
 
